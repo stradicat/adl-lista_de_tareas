d007a41f3da137e9c227afc90963f91ac71bd861
Dennis Mayr <dmayr.dev@gmail.com>
2025-07-24 18:12:28 -0400

    Tarea: data class para cada tarea; id, etiqueta, importancia, descripción

--- app/src/main/java/dev/dmayr/listadetareas/model/Tarea.kt
+++ app/src/main/java/dev/dmayr/listadetareas/model/Tarea.kt
@@ -1,3 +1,16 @@
 package dev.dmayr.listadetareas.model
 
-class Tarea
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity("lista_de_tareas")
+data class Tarea(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val etiqueta: Int,
+    val importancia: Byte, // para 1..5 es más que suficiente
+    val descripcionTarea: String
+) : Parcelable
