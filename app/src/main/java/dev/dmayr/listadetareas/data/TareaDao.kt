c43b0098a67a565f874359b0c10e25ba9c3edcd0
Dennis Mayr <dmayr.dev@gmail.com>
2025-07-24 18:15:35 -0400

    TareaDao: agregado método para eliminar todas las tareas

--- app/src/main/java/dev/dmayr/listadetareas/data/TareaDao.kt
+++ app/src/main/java/dev/dmayr/listadetareas/data/TareaDao.kt
@@ -2,16 +2,28 @@ package dev.dmayr.listadetareas.data
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
 import dev.dmayr.listadetareas.model.Tarea
 
 @Dao
 interface TareaDao {
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun agregarTarea(tarea: Tarea)
 
+    @Update
     suspend fun modificarTarea(tarea: Tarea)
 
+    @Delete
     suspend fun eliminarTarea(tarea: Tarea)
 
-    suspend fun mostrarTareas(): LiveData<Tarea>
+    @Query("DELETE from lista_de_tareas")
+    suspend fun eliminarTodasLasTareas()
+
+    @Query("SELECT * from lista_de_tareas ORDER BY id ASC")
+    fun mostrarTareas(): LiveData<Tarea>
 }
