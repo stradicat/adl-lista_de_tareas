03b1e0c94319be248011406a7c1551139f5fc201
Dennis Mayr <dmayr.dev@gmail.com>
2025-07-28 22:49:54 -0400

    MainActivity: implementación de SharedPreferences y métodos de guardado y lectura.

--- app/src/main/java/dev/dmayr/listadetareas/MainActivity.kt
+++ app/src/main/java/dev/dmayr/listadetareas/MainActivity.kt
@@ -1,9 +1,11 @@
 package dev.dmayr.listadetareas
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
@@ -15,12 +17,21 @@ import dev.dmayr.listadetareas.model.Tarea
 import dev.dmayr.listadetareas.utils.SwipeToDeleteCallback
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val PREFERENCIAS = "prefs_lista_de_tareas"
+    }
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var tareasAdapter: TareasAdapter
     private val listaDeTareas: MutableList<Tarea> = mutableListOf()
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences = getSharedPreferences(
+            PREFERENCIAS, MODE_PRIVATE
+        )
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val view = binding.root
@@ -43,8 +54,13 @@ class MainActivity : AppCompatActivity() {
         tareasAdapter = TareasAdapter(listaDeTareas) {
             updatecount(tvTareasPendientes)
         }
+
+        leerTodasLasTareas()
+        updatecount(tvTareasPendientes)
+
         val handleElementSwipes = SwipeToDeleteCallback { pos ->
             tareasAdapter.borrarTarea(pos)
+            borrarTarea(pos)
             updatecount(tvTareasPendientes)
         }
         rvTareas.layoutManager = LinearLayoutManager(this)
@@ -57,6 +73,7 @@ class MainActivity : AppCompatActivity() {
             if (texto.isNotEmpty()) {
                 val nuevaTarea = Tarea(System.currentTimeMillis(), texto)
                 tareasAdapter.agregarTarea(nuevaTarea)
+                guardarTarea(nuevaTarea)
                 descripcionTarea.text.clear()
                 updatecount(tvTareasPendientes)
             }
@@ -64,7 +81,53 @@ class MainActivity : AppCompatActivity() {
 
         btnBorrarTodasLasTareas.setOnClickListener {
             tareasAdapter.borrarTodasLasTareas()
+            borrarTodasLasTareas()
             updatecount(tvTareasPendientes)
+        }
+    }
+
+    private fun guardarTarea(nuevaTarea: Tarea) {
+        val tareaKey = nuevaTarea.id.toString()
+        sharedPreferences.edit {
+            putLong("${tareaKey}_id", nuevaTarea.id)
+                .putString("${tareaKey}_descripcion", nuevaTarea.descripcionTarea)
+                .putBoolean("${tareaKey}_isComplete", nuevaTarea.isComplete)
+        }
+    }
+
+    private fun borrarTarea(pos: Int) {
+        if (pos in listaDeTareas.indices) {
+            val tareaKey = listaDeTareas[pos].id.toString()
+            sharedPreferences.edit {
+                remove("${tareaKey}_id")
+                remove("${tareaKey}_descripcion")
+                remove("${tareaKey}_isComplete")
+            }
+        }
+    }
+
+    private fun borrarTodasLasTareas() {
+        sharedPreferences.edit { clear() }
+    }
+
+    private fun leerTodasLasTareas() {
+        listaDeTareas.clear()
+
+        val todasLasPreferencias = sharedPreferences.all
+        val tareaIds = todasLasPreferencias.keys
+            .filter { it.endsWith("_id") }
+            .map { it.removeSuffix("_id") }
+            .distinct()
+
+        tareaIds.forEach { idStr ->
+            val id = sharedPreferences.getLong("${idStr}_id", -1)
+            val descripcion = sharedPreferences.getString("${idStr}_descripcion", null)
+            val isComplete = sharedPreferences.getBoolean("${idStr}_isComplete", false)
+
+            if (id != (-1).toLong() && descripcion != null) {
+                val tarea = Tarea(id, descripcion, System.currentTimeMillis(), isComplete)
+                listaDeTareas.add(tarea)
+            }
         }
     }
 
