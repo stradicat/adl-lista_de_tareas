1b75d3a2dea0ac9348896db349fd8471609f2313
Dennis Mayr <dmayr.dev@gmail.com>
2025-07-28 20:46:20 -0400

    MainActivity: implementación inicial de dataBindings y adapter

--- app/src/main/java/dev/dmayr/listadetareas/MainActivity.kt
+++ app/src/main/java/dev/dmayr/listadetareas/MainActivity.kt
@@ -1,16 +1,20 @@
 package dev.dmayr.listadetareas
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import dev.dmayr.listadetareas.adapter.TareasAdapter
 import dev.dmayr.listadetareas.databinding.ActivityMainBinding
+import dev.dmayr.listadetareas.model.Tarea
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: TareasAdapter
+    private val listaDeTareas: MutableList<Tarea> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +30,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        /*-----------------------------------------------------------------------*/
+        binding.descripcionTarea
+        binding.btnAgregarTarea
+        binding.btnBorrarTodasLasTareas
+        binding.rvTareas
+        val tvTareasPendientes = binding.tvTareasPendientes
+
+        adapter = TareasAdapter(listaDeTareas) {
+            updatecount(tvTareasPendientes)
+        }
+    }
+
+    private fun updatecount(tareasPendientes: TextView) {
+        val pendientes: String = adapter.getTareas().count { !it.isComplete }.toString()
+        tareasPendientes.text = pendientes
     }
 }
