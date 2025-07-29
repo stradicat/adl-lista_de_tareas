55cc3bc8cba44fce6851c09eeabc9725b4cfc68f
Dennis Mayr <dmayr.dev@gmail.com>
2025-07-28 20:44:48 -0400

    Implementación de TareasAdapter

--- app/src/main/java/dev/dmayr/listadetareas/adapter/TareasAdapter.kt
+++ app/src/main/java/dev/dmayr/listadetareas/adapter/TareasAdapter.kt
@@ -1,3 +1,65 @@
 package dev.dmayr.listadetareas.adapter
 
-class TareasAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dev.dmayr.listadetareas.databinding.ItemTareaBinding
+import dev.dmayr.listadetareas.model.Tarea
+
+class TareasAdapter(
+    private val listaDeTareas: MutableList<Tarea>,
+    private val onCheckedChanged: (Tarea) -> Unit
+) :
+    RecyclerView.Adapter<TareasAdapter.TareasViewHolder>() {
+    private lateinit var binding: ItemTareaBinding
+//    private lateinit var listaDeTareas: MutableList<Tarea>
+
+    inner class TareasViewHolder(binding: ItemTareaBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val descripcionTarea = binding.descripcionTarea
+        val cbTareaTerminada = binding.cbTareaTerminada
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TareasViewHolder {
+
+        binding = ItemTareaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TareasViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: TareasViewHolder,
+        position: Int
+    ) {
+        val tarea = listaDeTareas[position]
+        holder.descripcionTarea.text = tarea.descripcionTarea
+        holder.cbTareaTerminada.isChecked = tarea.isComplete
+        holder.cbTareaTerminada.setOnCheckedChangeListener(null)
+        holder.cbTareaTerminada.setOnCheckedChangeListener { _, isChecked ->
+            tarea.isComplete = isChecked
+            onCheckedChanged(tarea)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listaDeTareas.size
+    }
+
+    fun borrarTarea(pos: Int) {
+        listaDeTareas.removeAt(pos)
+        notifyItemRemoved(pos)
+    }
+
+    fun borrarTodasLasTareas() {
+        listaDeTareas.removeAll(listaDeTareas)
+    }
+
+    fun agregarTarea(tarea: Tarea) {
+        listaDeTareas.add(0, tarea)
+        notifyItemInserted(0)
+    }
+
+    fun getTareas(): List<Tarea> = listaDeTareas
+}
