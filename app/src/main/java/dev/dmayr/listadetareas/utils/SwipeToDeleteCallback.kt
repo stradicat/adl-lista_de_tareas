bcf3b0a37fe51c02b27a844f7282e4e07e2fe13f
Dennis Mayr <dmayr.dev@gmail.com>
2025-07-28 20:43:54 -0400

    Agregado `SwipeToDeleteCallback`

--- app/src/main/java/dev/dmayr/listadetareas/utils/SwipeToDeleteCallback.kt
+++ app/src/main/java/dev/dmayr/listadetareas/utils/SwipeToDeleteCallback.kt
@@ -1,3 +1,19 @@
 package dev.dmayr.listadetareas.utils
 
-class SwipeToDeleteCallback
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+class SwipeToDeleteCallback(
+    private val onSwipedAction: (position: Int) -> Unit
+) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ) = false
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        onSwipedAction(viewHolder.adapterPosition)
+    }
+}
